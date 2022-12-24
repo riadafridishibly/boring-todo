1f499cb8f217b7b5674d23c1a3f84d9d8c8fd7b9
Riad <riadafridishibly@gmail.com>
2022-12-24 12:56:34 +0600

    Add systray
    - Open and quit application from systray

--- go.mod
+++ go.mod
@@ -4,19 +4,29 @@ go 1.18
 
 require (
 	github.com/Masterminds/squirrel v1.5.3
+	github.com/getlantern/systray v1.2.1
 	github.com/go-chi/chi/v5 v5.0.8
 	github.com/go-chi/cors v1.2.1
 	github.com/go-chi/render v1.0.2
 	github.com/k0kubun/pp/v3 v3.2.0
 	github.com/mattn/go-sqlite3 v1.14.9
+	github.com/skratchdot/open-golang v0.0.0-20200116055534-eef842397966
 )
 
 require (
 	github.com/ajg/form v1.5.1 // indirect
+	github.com/getlantern/context v0.0.0-20190109183933-c447772a6520 // indirect
+	github.com/getlantern/errors v0.0.0-20190325191628-abdb3e3e36f7 // indirect
+	github.com/getlantern/golog v0.0.0-20190830074920-4ef2e798c2d7 // indirect
+	github.com/getlantern/hex v0.0.0-20190417191902-c6586a6fe0b7 // indirect
+	github.com/getlantern/hidden v0.0.0-20190325191715-f02dbb02be55 // indirect
+	github.com/getlantern/ops v0.0.0-20190325191751-d70cb0d6f85f // indirect
+	github.com/go-stack/stack v1.8.0 // indirect
 	github.com/lann/builder v0.0.0-20180802200727-47ae307949d0 // indirect
 	github.com/lann/ps v0.0.0-20150810152359-62de8c46ede0 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.16 // indirect
+	github.com/oxtoacart/bpool v0.0.0-20190530202638-03653db5a59c // indirect
 	github.com/stretchr/testify v1.7.0 // indirect
 	golang.org/x/sys v0.0.0-20220811171246-fbc7d0a398ab // indirect
 	golang.org/x/text v0.3.7 // indirect
--- main.go
+++ main.go
@@ -1,15 +1,22 @@
 package main
 
 import (
+	"context"
+	"fmt"
 	"io/fs"
 	"log"
 	"net/http"
 	"strings"
+	"time"
 
+	"github.com/getlantern/systray"
 	"github.com/go-chi/chi/v5"
 	"github.com/riadafridishibly/boring-todo/api"
 	"github.com/riadafridishibly/boring-todo/frontend"
+	"github.com/skratchdot/open-golang/open"
 )
+
+const SITE_URL = "http://localhost:8989/"
 
 func FileServer(r chi.Router, path string, root http.FileSystem) {
 	if strings.ContainsAny(path, "{}*") {
@@ -30,7 +37,28 @@ func FileServer(r chi.Router, path string, root http.FileSystem) {
 	})
 }
 
-func main() {
+func handleOpen(m *systray.MenuItem) {
+	go func() {
+		for range m.ClickedCh {
+			open.Run(SITE_URL)
+		}
+	}()
+}
+
+func handleQuit(m *systray.MenuItem) {
+	go func() {
+		for range m.ClickedCh {
+			systray.Quit()
+		}
+	}()
+}
+
+var server *http.Server
+
+func newServer() *http.Server {
+	srv := &http.Server{}
+	srv.Addr = ":8989"
+
 	todoApi, err := api.NewTodoAPI("test.db")
 	if err != nil {
 		log.Fatal(err)
@@ -43,5 +71,37 @@ func main() {
 	}
 	FileServer(routes, "/", http.FS(distFS))
 
-	http.ListenAndServe(":8989", routes)
+	srv.Handler = routes
+
+	return srv
+}
+
+func onReady() {
+	systray.SetTitle("TODO")
+	systray.SetTooltip("Boring todos")
+	o := systray.AddMenuItem("Open", "Open boring todo application")
+	handleOpen(o)
+
+	q := systray.AddMenuItem("Quit", "Quit the boring application")
+	handleQuit(q)
+
+	server = newServer()
+	go func() {
+		err := server.ListenAndServe()
+		if err == http.ErrServerClosed {
+			fmt.Println("Server shutdown successful")
+		} else {
+			log.Println(err)
+		}
+	}()
+}
+
+func onExit() {
+	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+	defer cancel()
+	server.Shutdown(ctx)
+}
+
+func main() {
+	systray.Run(onReady, onExit)
 }
